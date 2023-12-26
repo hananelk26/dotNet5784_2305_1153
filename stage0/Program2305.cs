e114a0d9a54fbd5ae5b3443551bcb6e74198517d
hananelk26 <hananelk26@gmail.com>
2023-12-26 21:17:01 +0200

    Initial Code + Welcome functions

---
+++ stage0/Program2305.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace targil0
+{ 
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome2305();
+            Welcome1153();
+        }
+
+        static partial void Welcome1153();
+        private static void Welcome2305()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(userName + ", welcome to my first console application");
+        }
+    }
+}
