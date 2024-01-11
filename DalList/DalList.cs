14d070b3e9b66af57d03cceb5033223ce40faa98
NoamPeretzJct <noam.forwork@gmail.com>
2024-01-11 10:11:33 +0200

    ADD DALLIST CLASS

---
+++ DalList/DalList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+using DalApi;
+using System.Diagnostics.CodeAnalysis;
+
+sealed public class DalList : IDal
+{
+    public IDependency Dependency => new DependencyImplementation();
+
+    public IEngineer Engineer =>  new EngineerImplementation();
+
+    public ITask Task => new TaskImplementation();
+}
