63d6d7dcea6223a096575d436f2e38ecd76eb5f1
NoamPeretzJct <noam.forwork@gmail.com>
2024-01-21 16:38:59 +0200

    implementation crud

---
+++ DalXml/DependencyImplementation.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class DependencyImplementation : IDependency
+{
+    readonly string s_dependency_xml = "dependnecy";
+
+    public int Create(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(Func<Dependency, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Dependency?> ReadAll(Func<Dependency, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+}
