758b5891d317e0ba5de5832c873283bcb68863df
NoamPeretzJct <noam.forwork@gmail.com>
2024-01-21 15:11:35 +0200

    working on xml

---
+++ DalXml/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal;
+
+internal static class Config
+{
+    static string s_data_config_xml = "data-config";
+    internal static int NextCourseId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextTaskId"); }
+    internal static int NextLinkId { get => XMLTools.GetAndIncreaseNextId(s_data_config_xml, "NextDependencyId"); }
+}
