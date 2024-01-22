6a8c44cfd8d4e039a4fe4c310a6ebb7ed0d9e24c
hananelk26 <hananelk26@gmail.com>
2024-01-22 23:54:10 +0200

    Finish stage 3

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -3,6 +3,7 @@ using DalApi;
 using DO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace DalTest;
@@ -414,7 +415,27 @@ internal class Program
         }
     }
 
-    
+    public static void resetDataConfig()
+    {
+        XElement ?ex = null;
+
+        const string s_xml_dir = @"..\xml\";
+        string filePath = $"{s_xml_dir + "data-config"}.xml";
+        try
+        {
+            if (File.Exists(filePath))
+               ex =  XElement.Load(filePath);
+            
+        }
+        catch (Exception eex)
+        {
+            throw new DalXMLFileLoadCreateException($"fail to load xml file: {s_xml_dir + filePath}, {eex.Message}");
+        }
+        int num = 1;
+        ex.Element("NextTaskId")!.Value = num.ToString();
+        ex.Element("NextDependencyId")!.Value = num.ToString();
+        
+    }
 
     static void Main(string[] args)
     {
@@ -425,6 +446,7 @@ internal class Program
             s_dal.Engineer.DeleteAll();
             s_dal.Task.DeleteAll();
             s_dal.Dependency.DeleteAll();
+            resetDataConfig();
             Initialization.Do(s_dal);
         }
         int choice = 0;
