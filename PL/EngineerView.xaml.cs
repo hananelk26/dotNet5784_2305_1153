505b17650ab5e3ecae728627b5ba9b173e72acd3
NoamPeretzJct <noam.forwork@gmail.com>
2024-03-03 16:45:05 +0200

    work on stage 6

--- PL/EngineerView.xaml.cs
+++ PL/EngineerView.xaml.cs
@@ -1,5 +1,8 @@
-﻿using PL.Engineer;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,23 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for EngineerView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class EngineerView : Window
     {
-        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-        public MainWindow()
+        public EngineerView()
         {
             InitializeComponent();
-           
         }
-
-       
     }
 }
---
+++ PL/ManagerView.xaml.cs
@@ -0,0 +1,87 @@
+﻿using PL.Engineer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+  
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ManagerView : Window
+    {
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public ManagerView()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        /// <summary>
+        /// Event handler for the EngineerListWindow button click.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">The event data.</param>
+        private void EngineerListWindow(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
+        }
+
+        /// <summary>
+        /// Event handler for the Initialization button click.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">The event data.</param>
+        private void Initialization(object sender, RoutedEventArgs e)
+        {
+
+            MessageBoxResult mbResult = MessageBox.Show("Would you like to create Initial data?",
+                            "warning",
+                            MessageBoxButton.YesNoCancel,
+                            MessageBoxImage.Question);
+            switch (mbResult)
+            {
+                case MessageBoxResult.Yes:
+                    //  DalTest.Initialization.Do();
+                    s_bl.InitializeDB();
+                    break;
+
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the resetAllData button click.
+        /// </summary>
+        /// <param name="sender">The object that raised the event.</param>
+        /// <param name="e">The event data.</param>
+        private void resetAllData(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mbResult = MessageBox.Show("Would you like to reset all data?",
+                            "warning",
+                            MessageBoxButton.YesNoCancel,
+                            MessageBoxImage.Warning);
+            switch (mbResult)
+            {
+                case MessageBoxResult.Yes:
+                    s_bl.ResetsAllEntitiesInTheData();
+                    s_bl.resetDataConfig();
+                    break;
+
+            }
+
+
+        }
+    }
+}
