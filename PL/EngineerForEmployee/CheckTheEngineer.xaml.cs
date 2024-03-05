d24738926ec855f8b26f07e679e9d445e7225ed0
NoamPeretzJct <noam.forwork@gmail.com>
2024-03-05 16:36:39 +0200

    work on stage 6

--- PL/EngineerForEmployee/CheckTheEngineer.xaml.cs
+++ PL/EngineerForEmployee/CheckTheEngineer.xaml.cs
@@ -12,16 +12,74 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PL.EngineerForEmployee
+namespace PL.EngineerForEmployee;
+
+/// <summary>
+/// Interaction logic for CheckTheEngineer.xaml
+/// </summary>
+/// 
+using System.ComponentModel;
+
+public class EmployeeViewModel : INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for CheckTheEngineer.xaml
-    /// </summary>
-    public partial class CheckTheEngineer : Window
+    private string _employeeId;
+
+    public string EmployeeId
     {
-        public CheckTheEngineer()
+        get => _employeeId;
+        set
         {
-            InitializeComponent();
+            if (_employeeId != value)
+            {
+                _employeeId = value;
+                OnPropertyChanged(nameof(EmployeeId));
+            }
         }
     }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
+public partial class CheckTheEngineer : Window
+{
+    private EmployeeViewModel _viewModel;
+    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+    public CheckTheEngineer()
+    {
+        InitializeComponent();
+        _viewModel = new EmployeeViewModel();
+        DataContext = _viewModel;
+    }
+
+    private void ContinueButton_Click(object sender, RoutedEventArgs e)
+    {
+        string idEngineer = _viewModel.EmployeeId;
+        try
+        {
+           var en = s_bl.Engineer.Read(int.Parse(idEngineer));
+            if (en != null)
+            {
+                new MainWindowEngineer().ShowDialog();
+            }
+        }
+        catch (Exception ex) 
+        {
+            MessageBox.Show(ex.Message,"ERROR",MessageBoxButton.OK,MessageBoxImage.Error);
+        }
+
+    }
+
+    private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        e.Handled = !e.Text.All(char.IsDigit);
+    }
+
+
 }
