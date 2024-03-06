da0b2634a935332a4d214edd696943a57b9079d9
NoamPeretzJct <noam.forwork@gmail.com>
2024-03-06 17:12:11 +0200

    WORK

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -67,7 +67,7 @@ class ConvertDateForProjectToIsEnabled : IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        MessageBox.Show($"{value is bool}");
+       
         if (value is bool boolValue)
         {
             return !boolValue;
@@ -80,4 +80,11 @@ class ConvertDateForProjectToIsEnabled : IValueConverter
     {
         throw new NotImplementedException();
     }
+
+   
+}
+
+class ConvertDateForProjectToIsEnabled : IValueConverter
+{
+
 }
